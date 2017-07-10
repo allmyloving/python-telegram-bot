f8201c523a18a8a752dfceb78a04ae4b0e08623c
Daria Serdiuk <daria.serdiuk13@gmail.com>
2017-07-10 10:55:15 +0300

    Use library instead of straight forward approach

--- dbmanager.py
+++ dbmanager.py
@@ -3,12 +3,12 @@ from pymongo import MongoClient
 DB_URL = "mongodb://admin:admin@ds023325.mlab.com:23325/heroku_drc033gr"
 
 
-def add():
+def add(text):
     client = MongoClient(DB_URL)
     db = client.heroku_drc033gr
     result = db.rem.insert_one({
         'chat_id': '123',
         'time': '1111',
-        'text': 'reminder'
+        'text': text
     })
     print(result.inserted_id)
--- main.py
+++ main.py
@@ -1,39 +1,29 @@
-import requests
-import json
 import random
-from flask import Flask, request
-from dbmanager import add
+
+import requests
+import telebot
+from flask import Flask
+
+import dbmanager
 
 app = Flask(__name__)
 
 TOKEN = '260039213:AAHHKne9Sme8r-fROdHRBk577_o7XZblBcQ'
 URL = "https://api.telegram.org/bot%s/" % TOKEN
-APP_URL = "https://morning-fortress-78667.herokuapp.com/"
+# APP_URL = "https://morning-fortress-78667.herokuapp.com/"
+
+bot = telebot.TeleBot(TOKEN)
+
+COMMANDS = ["/start", "/help", "/benice", "/addrem"]
+NICE_MSG = ["You look great today!",
+            "You're gonna be fine!",
+            "Love and peace",
+            "Keep calm and learn Java ~~~~"]
 
 
 @app.route('/hello')
 def hello_world():
     return 'Hello World!'
-
-
-@app.route('/', methods=['POST'])
-def main():
-    update = json.loads(request.data)
-    print(update)
-    print(update['message'].get('text'))
-    message = update['message']
-
-    response = execute_comm(message.get('text'), message)
-    print(response)
-    send_response(response, message['chat']['id'])
-    return "OK"
-
-
-def execute_comm(command, message):
-    comm = COMMANDS.get(message.get('text'))
-    if comm is None:
-        return "Error, command %s not found" % command
-    return comm(message)
 
 
 def send_response(response, chat_id):
@@ -42,26 +32,30 @@ def send_response(response, chat_id):
     requests.post(URL + "sendMessage", data)
 
 
+@bot.message_handler(commands=['start'])
 def start(message):
-    return "Hello there %s!" % get_user_name(message)
+    bot.reply_to(message, "Hello there %s!" % get_user_name(message))
 
 
+@bot.message_handler(commands=['help'])
 def help_comm(message):
     # response = {'chat_id': message['chat']['id']}
     result = ["Hello %s! \r\nI can accept only these commands:\r\n" % get_user_name(message)]
     for cmd in COMMANDS:
         result.append(cmd)
-    return "\n\t".join(result)
+    bot.reply_to(message, "\n\t".join(result))
 
 
+@bot.message_handler(commands=['be_nice'])
 def be_nice(message):
     index = random.randint(0, len(NICE_MSG) - 1)
-    return NICE_MSG[index]
+    bot.reply_to(message, NICE_MSG[index])
 
 
+@bot.message_handler(commands=['addrem'])
 def add_rem(message):
-    add()
-    return 'completed'
+    dbmanager.add(message)
+    bot.reply_to(message, 'completed')
 
 
 def get_user_name(message):
@@ -70,24 +64,9 @@ def get_user_name(message):
 
 @app.route('/bot')
 def call_bot():
-    req = requests.get(URL + "setWebhook?url=%s" % APP_URL)
+    req = requests.post(URL + "setWebhook?url=%s" % '')
     print(req.status_code)
     return req.status_code == 200
 
-
-if call_bot():
-    print("all ok")
-
-COMMANDS = {"/start": start,
-            "/help": help_comm,
-            "/benice": be_nice,
-            "/addrem": add_rem}
-NICE_MSG = ["You look great today!",
-            "You're gonna be fine!",
-            "Love and peace",
-            "Keep calm and learn Java ~~~~"]
-
-if __name__ == '__main__':
-    app.run()
-    if call_bot():
-        print("All ok")
+call_bot()
+bot.polling()
